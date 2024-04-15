86848e1e764d8a228db4f2522b51e7e88e065a0f
Aubry Nobel Niyonkuru <1944238@csfoy.ca>
2024-04-14 22:53:43 -0400

    Components and containers added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+
 const App = () => {
   return (
-    <div>
-      <h1>Web apps</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 };
