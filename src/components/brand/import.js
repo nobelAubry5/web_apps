8893746721a4ff7103f8617a9b52b868c2c1f09b
Aubry Nobel Niyonkuru <1944238@csfoy.ca>
2024-04-15 15:40:13 -0400

    Application finished and online nobelaubry5.com

--- src/components/brand/import.js
+++ src/components/brand/import.js
@@ -3,5 +3,4 @@ import slack from "../../assets/slack.png";
 import atlassian from "../../assets/atlassian.png";
 import shopify from "../../assets/shopify.png";
 import dropbox from "../../assets/dropbox.png";
-
 export { google, slack, atlassian, shopify, dropbox };
